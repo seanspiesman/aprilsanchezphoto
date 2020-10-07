7857eec519462bc9b3a78613e91684dd9668da52
comatthewb <58574067+comatthewb@users.noreply.github.com>
2020-10-07 17:57:50 -0500

    added data to data file

--- src/generateFakeData.js
+++ src/generateFakeData.js
@@ -1,13 +1,18 @@
 const faker = require("faker");
+const fs = require("fs");
 
 function seedFakeData(n) {
   let imageObj = {};
   for (let i = 0; i < n; i++) {
     imageObj[i] = faker.image.image();
   }
-  return imageObj;
+  console.log(JSON.stringify(imageObj));
+  fs.writeFile("data.json", JSON.stringify(imageObj), (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
 }
 
-let imageData = Object.values(seedFakeData(100));
+console.log(seedFakeData(10));
 
-module.exports = { imageData };
+// let imageData = Object.values(seedFakeData(100));
